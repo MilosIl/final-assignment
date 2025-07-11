71e076af81a06e05587f8186f869c079aebc1deb
Milos <milos.ilic20@hotmail.com>
2025-07-11 11:47:20 +0200

    feat: product and profile page added to routing

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,19 @@
 import { HomePage, LoginPage } from '@/pages';
+import { ProductPage } from '@/pages/ProductPage/ProductPage';
+import { ProfilePage } from '@/pages/ProfilePage/ProfilePage';
 
 const AppRouter = [
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/products',
+    element: <ProductPage />,
+  },
+  {
+    path: '/profile/:id',
+    element: <ProfilePage />,
   },
   {
     path: '/login',
